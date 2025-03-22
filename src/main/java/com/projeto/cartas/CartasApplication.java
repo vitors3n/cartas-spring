194f4ca0d0b987fa3b836fa3325398373edf00c7
Vitor S. <svavitor.dev@gmail.com>
2025-03-22 12:45:57 -0300

    inicio do projeto

---
+++ src/main/java/com/projeto/cartas/CartasApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.cartas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartasApplication.class, args);
+	}
+
+}
