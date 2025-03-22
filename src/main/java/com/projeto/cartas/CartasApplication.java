3d961a45a86528ec2d9910f5239dad5baff04030
Vitor S. <svavitor.dev@gmail.com>
2025-03-22 13:13:00 -0300

    entendendo funcionamento de um controller e um service

--- src/main/java/com/projeto/cartas/CartasApplication.java
+++ src/main/java/com/projeto/cartas/CartasApplication.java
@@ -1,9 +1,14 @@
 package com.projeto.cartas;
 
+import com.projeto.cartas.carta.Card;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class CartasApplication {
 
 	public static void main(String[] args) {
