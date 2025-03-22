4dd9f464f53038192c62d63b045f8403f83dd6b7
Vitor S. <svavitor.dev@gmail.com>
2025-03-22 14:07:36 -0300

    GET e POST configurados com o banco de dados postgreSQL

---
+++ src/main/java/com/projeto/cartas/carta/CardConfig.java
@@ -0,0 +1,23 @@
+package com.projeto.cartas.carta;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class CardConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(CardRepository repository) {
+        return args -> {
+            Card artigo1 = new Card("Artigo 1", "Texto", 0);
+            Card artigo2 = new Card("Artigo 2", "Texto", 0);
+
+            repository.saveAll(
+                    List.of(artigo1, artigo2)
+            );
+        };
+    }
+}
---
+++ src/main/java/com/projeto/cartas/carta/CardRepository.java
@@ -0,0 +1,15 @@
+package com.projeto.cartas.carta;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CardRepository
+        extends JpaRepository<Card, Long> {
+
+    //@Query("SELECT c FROM card c WHERE c.front = ?1")
+    Optional<Card> findCardByFront(String front);
+}
