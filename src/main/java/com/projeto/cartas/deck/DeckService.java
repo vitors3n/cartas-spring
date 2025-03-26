5514aeecc02f1b70a28dee1df2afc9a5cd819c5a
Vitor S. <svavitor.dev@gmail.com>
2025-03-25 21:46:38 -0300

    DeckService agora reporta melhor cada resposta com seu devido statuscode

--- src/main/java/com/projeto/cartas/deck/DeckService.java
+++ src/main/java/com/projeto/cartas/deck/DeckService.java
@@ -1,8 +1,12 @@
 package com.projeto.cartas.deck;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.lang.module.ResolutionException;
 import java.util.List;
 import java.util.Optional;
 
@@ -20,15 +24,23 @@ public class DeckService {
     }
 
     public Optional<Deck> getDeckbyId(Long deckId){
+        Optional<Deck> deck = deckRepository.findById(deckId);
+
+        if (deck.isEmpty()){
+           throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Deck não encontrado.");
+        }
+
         return deckRepository.findById(deckId);
     }
 
     public void addNewDeck(Deck deck) {
         Optional<Deck> deckOptional = deckRepository.findDeckByName(deck.getName());
         if (deckOptional.isPresent()){
-            throw new IllegalStateException("Deck já existe");
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "Deck já existe.");
         }
         deckRepository.save(deck);
+
+        throw new ResponseStatusException(HttpStatus.CREATED);
     }
 
     public void deleteDeck(Long deckId){
